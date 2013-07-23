3ff0f95108d4c4a728d1dfce59eb82fdc6abd142
convict666 <milosh67@gmail.com>
2013-07-23 14:48:57 +0200

    coupon subtotal fix, now sale prices are counted

--- classes/cart.class.php
+++ classes/cart.class.php
@@ -1067,7 +1067,9 @@ class Cart {
 					}
 					$this->basket['contents'][$hash]['quantity'] = $quantity;
 					$product_data['product_id'] = (int)$this->basket['contents'][$hash]['id'];
-					$this->basket['contents'][$hash]['total_price_each'] = ($product['price']+$this->basket['contents'][$hash]['option_line_price']);
+					//$this->basket['contents'][$hash]['total_price_each'] = ($product['price']+$this->basket['contents'][$hash]['option_line_price']);
+					$pprice = $product['ctrl_sale'] ? $product['sale_price'] : $product['price'];
+					$this->basket['contents'][$hash]['total_price_each'] = ($pprice+$this->basket['contents'][$hash]['option_line_price']);
 					
 					$this->_subtotal += $this->basket['contents'][$hash]['total_price_each'] * $quantity;
 					$this->basket['subtotal'] = $this->_subtotal;
