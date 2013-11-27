ba83aced9bd019a4229e005fc61f891c6eb32781
Al Brookbanks <al@fathem.com>
2013-11-27 10:59:01 +0000

    Fixed issue #16

--- modules/plugins/Amazon_Checkout/skin/button.php
+++ modules/plugins/Amazon_Checkout/skin/button.php
@@ -1,5 +1,7 @@
-<script type='text/javascript' src='{$AMAZON.widgetURL}'>
-</script>
+<noscript>
+<div style="color: red; font-weight: bold; width: 75%;">Your browser does not support JavaScript which is required to pay with Amazon. Please enable it or use a different browser.</div>
+</noscript>
+<script type='text/javascript' src='{$AMAZON.widgetURL}'></script>
 <div id="AmazonInlineWidget" align="right"><img src="https://{$AMAZON.redirectDomain}/gp/cba/button?cartOwnerId={$AMAZON.merchId}&size={$AMAZON.buttonSize}&color={$AMAZON.buttonColor}&background={$AMAZON.buttonBg}&type=inlineCheckout" style="cursor: pointer;"/></div>
 
 <script type='text/javascript' >
