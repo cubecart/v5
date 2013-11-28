4f59e248a78c2b4f2f61f85750b28bfb655c4eb3
Al Brookbanks <al@fathem.com>
2013-11-28 13:54:57 +0000

    Fixed mantis issue 639

--- admin/sources/products.import.inc.php
+++ admin/sources/products.import.inc.php
@@ -134,7 +134,7 @@ if (isset($_POST['process'])) {
 					}
 					// Insert SEO custom URL
 					if (empty($product_record['seo_path'])) $product_record['seo_path'] = $GLOBALS['seo']->generatePath($product_id, 'prod');
-					$db->insert('CubeCart_seo_urls', array('path'=> sanitizeSEOPath($product_record['seo_path']), 'item_id' => $product_id, 'type' => 'prod'));
+					$GLOBALS['db']->insert('CubeCart_seo_urls', array('path'=> sanitizeSEOPath($product_record['seo_path']), 'item_id' => $product_id, 'type' => 'prod'));
 				}
 				unset($product_record, $category_record, $image_record, $image);
 			}
