17c5fa916051abadf7a829698f8153a0ce771211
Milos Homola <milosh67@gmail.com>
2014-01-26 22:57:05 +0100

    Fixed mcrypt detection in module setting
    Ian was correct in forum. "else" was missing there LOL.
    
    if (extension_loaded('mcrypt') && function_exists('mcrypt_module_open'))
    
    is correct as mcrypt loaded has mcrypt_module_open function cart code
    uses.

--- modules/gateway/Card_Capture/admin/index.inc.php
+++ modules/gateway/Card_Capture/admin/index.inc.php
@@ -4,4 +4,5 @@ $module		= new Module(__FILE__, $_GET['module'], 'admin/index.tpl', true);
 $page_content = $module->display();
 
 if (extension_loaded('mcrypt') && function_exists('mcrypt_module_open')) {}
+else
 $GLOBALS['main']->setACPWarning('Mcrypt library missing from server required to encrypt credit card data!');
