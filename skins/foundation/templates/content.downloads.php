bb03ff4ef5ad4349165c68976a6650efac0014f3
Al Brookbanks <al@fathem.com>
2014-02-20 18:09:01 +0000

    Downloads progress

--- skins/foundation/templates/content.downloads.php
+++ skins/foundation/templates/content.downloads.php
@@ -1,25 +1,38 @@
 {if $IS_USER}
 <h2>{$LANG.account.your_downloads}</h2>
 {$PAGINATION}
-
+  
+  <table>
+  <thead>
+  <tr>
+  	<td>Download Name</td>
+  	<td>Product Name</td>
+  	<td>{$LANG.account.download_count}</td>
+  	<td></td>
+  </tr>
+  </thead>
+  <tbody>
   {foreach from=$DOWNLOADS item=download}
+	  <tr>
 	  {if $download.deleted}
-	  {$LANG.account.download_deleted}
+	  <td colspan="">{$LANG.account.download_deleted}</td>
 	  {elseif $download.active}
-	  <a href="{$STORE_URL}/index.php?_a=download&amp;accesskey={$download.accesskey}" title="{$LANG.common.download}"><img src="{$STORE_URL}/skins/{$SKIN_FOLDER}/images/file_download.png" alt="{$LANG.common.download}" /></a>
-	  <a href="{$download.product_url}" title="{$LANG.catalogue.view_product}">{$download.name}</a>
-	<p><strong>{$LANG.account.download_expires}</strong>: {$download.expires} - <strong>{$LANG.account.download_count}</strong>: {$download.downloads}/{$MAX_DOWNLOADS}</p>
+	  <td><a href="{$STORE_URL}/index.php?_a=download&amp;accesskey={$download.accesskey}" title="{$LANG.common.download}"><i class="fa fa-download"> {$download.filename}</i></a></td>
+	  <td><a href="{$download.product_url}" title="{$LANG.catalogue.view_product}">{$download.name}</a></td>
+	  <td>{$download.expires}</td>
+	  <td>{$download.downloads}/{$MAX_DOWNLOADS}</td>
   
 		{else}
-  <div>
-	<span class="actions">{$LANG.common.expired}</span>
+  {$LANG.common.expired}
 	<a href="{$download.product_url}" title="View product">{$download.name}</a>
-	<p><strong>{$LANG.account.download_expired}</strong>: {$download.expires} - <strong>{$LANG.account.download_count}</strong>: {$download.downloads}/{$MAX_DOWNLOADS}</p>
-  </div>
-	  {/if}
+	<strong>{$LANG.account.download_expired}: {$download.expires} - <strong>{$LANG.account.download_count}</strong>: {$download.downloads}/{$MAX_DOWNLOADS}	  
+	{/if}
+	  </tr>
   {foreachelse}
   <div>{$LANG.notification.no_downloads_available}</div>
   {/foreach}
+  </tbody>
+  </table>
 
 {$PAGINATION}
 {else}
