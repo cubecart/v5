4607b5708d5841471664a439575312455351614b
Al Brookbanks <al@fathem.com>
2014-03-10 16:20:06 +0000

    Added Flag Sprites

--- skins/foundation/templates/box.language.php
+++ skins/foundation/templates/box.language.php
@@ -1,8 +1,8 @@
-<a href="#" id="language_switch">{$current_language.code}</a>
+<a href="#" id="language_switch"><img src="#" class="flag flag-{$current_language.code|substr:3:2}" alt="{$current_language.title}" /></a>
 <div class="hide pad" id="language_menu">
 <div>{$LANG.common.change_language}</div>
 {foreach from=$LANGUAGES item=language}
 <hr>
-<div class="text-left"><a href="{$language.url}">{$language.title}</a></div>
+<div class="text-left"><a href="{$language.url}"><img src="#" class="flag flag-{$language.code|substr:3:2}" alt="{$language.title}" /> {$language.title}</a></div>
 {/foreach}
 </div>
