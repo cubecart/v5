638ea1f672fce7abefa81142feb76747b091f825
Al Brookbanks <al@fathem.com>
2014-03-14 08:05:15 +0000

    Fixed suburb

--- modules/gateway/PayMate/gateway.class.php
+++ modules/gateway/PayMate/gateway.class.php
@@ -38,8 +38,8 @@ class Gateway {
 			'pmt_country'			=> $this->_basket['billing_address']['country_iso'],
 			'regindi_state'			=> $this->_basket['billing_address']['state'],
 			'regindi_address1'		=> $this->_basket['billing_address']['line1'],
-			'regindi_address2'		=> '',
-			'regindi_sub'			=>$this->_basket['billing_address']['line2'],
+			'regindi_address2'		=> $this->_basket['billing_address']['line2'],
+			'regindi_sub'			=> $this->_basket['billing_address']['town'],
 			'regindi_pcode'			=> $this->_basket['billing_address']['postcode'],
 			'return'				=> $GLOBALS['storeURL'].'/index.php?_g=rm&amp;type=gateway&amp;cmd=process&amp;module=PayMate'
 		);
