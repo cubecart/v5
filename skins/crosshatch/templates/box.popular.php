1b854f0a7766b92bb954419e5674e83f113e15bc
Al Brookbanks <al@fathem.com>
2014-04-10 11:05:57 +0100

    temp delete

--- skins/crosshatch/templates/box.popular.php
+++
@@ -1,8 +0,0 @@
-<div class="box">
-	<h3>{$LANG.catalogue.title_popular}</h3>
-	<ol>
-		 {foreach from=$POPULAR item=product}
-		<li><a href="{$product.url}" title="{$product.name}">{$product.name}</a></li>
-		 {/foreach}
-	</ol>
-</div>
--- skins/mobile/templates/content.document.php
+++
@@ -1,2 +0,0 @@
-<h2>{$DOCUMENT.doc_name}</h2>
-<div id="product_description">{$DOCUMENT.doc_content}</div>
