85523a6ec4fc49493891f8de0268ee0026b88267
Milos Homola <milosh67@gmail.com>
2014-05-13 10:21:50 +0200

    Gift certificate email sending fix
    Emails not being sent

--- classes/order.class.php
+++ classes/order.class.php
@@ -284,7 +284,8 @@ class Order {
 					if (!isset($complete)) {
 						$this->_getInventory($order_id);
 					}
-					if (!$this->_skip_order_complete_email && $this->_email_enabled) {
+					//if (!$this->_skip_order_complete_email && $this->_email_enabled) {
+					if ($this->_email_enabled) {
 						foreach ($this->_order_inventory as $item) {
 							// Send Gift Certificate
 							if (!empty($item['custom']) && !empty($item['coupon_id'])) {
