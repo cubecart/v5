cubecart/v5

commit e0d2ecf0d441d8106305dbba14937f00f3ae4777

PHP notice fix

controllers/controller.admin.session.true.inc.php
@@ -157,8 +157,8 @@ if (!empty($_GET['_g'])) {
 					}
 					$module_logo		= new Module(false, $module, false);
 					$module_info['title'] = $module_logo->module_fetch_logo($module_type, $module);
-					$module_info['mobile_optimized'] = (strtolower($xml->info->mobile_optimized)=="true") ? true : false;
-					if(!isset($xml->info->block) || $xml->info->block=='false') { $modules[] = $module_info; }
+					$module_info['mobile_optimized'] = ($xml && strtolower($xml->info->mobile_optimized)=="true") ? true : false;
+					if($xml && (!isset($xml->info->block) || $xml->info->block=='false')) { $modules[] = $module_info; }
 				}
 				$GLOBALS['smarty']->assign('MODULES', $modules);
 				$GLOBALS['smarty']->assign('ORDER_SELECT', $order_select);

commit 3475a6581a977ac7cb5d01bfe819476cd2d664dc

Fixed bug caused by e0d2ecf0d441d8106305dbba14937f00f3ae4777
Commit e0d2ecf0d441d8106305dbba14937f00f3ae4777 caused modules without
xml files to not load.

controllers/controller.admin.session.true.inc.php
@@ -158,7 +158,7 @@ if (!empty($_GET['_g'])) {
 					$module_logo		= new Module(false, $module, false);
 					$module_info['title'] = $module_logo->module_fetch_logo($module_type, $module);
 					$module_info['mobile_optimized'] = ($xml && strtolower($xml->info->mobile_optimized)=="true") ? true : false;
-					if($xml && (!isset($xml->info->block) || $xml->info->block=='false')) { $modules[] = $module_info; }
+					if(!isset($xml->info->block) || (isset($xml) && $xml->info->block=='false')) { $modules[] = $module_info; }
 				}
 				$GLOBALS['smarty']->assign('MODULES', $modules);
 				$GLOBALS['smarty']->assign('ORDER_SELECT', $order_select);