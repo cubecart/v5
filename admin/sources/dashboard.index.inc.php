f2d8a972c44b3fedae85fefb857df51e0877aab9
Al Brookbanks <al@fathem.com>
2014-08-15 11:41:29 +0100

    Fixed issue #131

--- admin/sources/dashboard.index.inc.php
+++ admin/sources/dashboard.index.inc.php
@@ -146,9 +146,20 @@ $unsettled_orders = $GLOBALS['db']->select('CubeCart_order_summary', array('cart
 if ($unsettled_orders) {
 	$tax = Tax::getInstance();
 	$GLOBALS['main']->addTabControl($lang['dashboard']['title_orders_unsettled'], 'orders', null, null, $unsettled_count);
+	
+	foreach($unsettled_orders as $order) {
+		$customer_ids[$order['customer_id']] = true;
+	}
+	$customers_in = implode(array_keys($customer_ids),',');
+	
+	$customers = $GLOBALS['db']->select('CubeCart_customer', array('type','customer_id'), 'customer_id IN ('.$customers_in.')');
+	foreach($customers as $customer) {
+		$customer_type[$customer['customer_id']] = $customer['type'];
+	}
+
 	foreach ($unsettled_orders as $order) {
 		$cart_order_ids[] = "'".$order['cart_order_id']."'";
-		$order['icon'] = (empty($order['customer_id'])) ? 'user_ghost' : 'user_registered';
+		$order['icon'] = $customer_type[$order['customer_id']]==1 ? 'user_registered' : 'user_ghost';
 		$order['date'] = formatTime($order['order_date']);
 		$order['total'] = Tax::getInstance()->priceFormat($order['total']);
 		$order['status'] = $lang['order_state']['name_'.$order['status']];
