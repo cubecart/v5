69d56d87a3ad3ffc734d35da5281e1b87c2102c3
Al Brookbanks <al@fathem.com>
2015-01-08 16:40:57 +0000

    Upgrade UTF8 conversion fix

--- setup/setup.upgrade.php
+++ setup/setup.upgrade.php
@@ -120,21 +120,21 @@ if (!isset($_SESSION['setup']['permissions'])) {
 			foreach ($tables as $table) {		
 			    ## Get Schema
 			    if ($schema = $db->misc('SHOW CREATE TABLE '.$table['Name'])) {
-			     	## Fix Schema and Create Temp Table
-				    $find 		= array("latin1", $table['Name']);
-				    $replace 	= array("utf8 COLLATE utf8_unicode_ci", $table['Name'].'_utf8');
+					## Fix Schema and Create Temp Table
+					$find   = array("latin1", $table['Name']);
+					$replace  = array("utf8", $table['Name'].'_utf8');
 
-				    $db->misc(str_replace($find, $replace, $schema[0]['Create Table']));
-					
-					if($GLOBALS['db']->misc("SHOW TABLES LIKE '".$table['Name']."_utf8'", false)) {					
-						## Copy Data
-						$db->misc('INSERT INTO '.$table['Name'].'_utf8 SELECT * FROM '.$table['Name']);
-						## Deleting Original Table
-						$db->misc('DROP TABLE '.$table['Name']);
-						## Renaming Temporary Table
-						$db->misc('ALTER TABLE '.$table['Name'].'_utf8 RENAME TO '.$table['Name']);
-					}  
-			    }
+					if($db->misc(str_replace($find, $replace, $schema[0]['Create Table']))) {
+						if ($db->misc("SHOW TABLES LIKE '".$table['Name']."_utf8'", false)) {
+							## Copy Data
+							$db->misc('INSERT INTO '.$table['Name'].'_utf8 SELECT * FROM '.$table['Name']);
+							## Deleting Original Table
+							$db->misc('DROP TABLE '.$table['Name']);
+							## Renaming Temporary Table
+							$db->misc('ALTER TABLE '.$table['Name'].'_utf8 RENAME TO '.$table['Name']);
+						}
+					}
+				}
 			}
 			$_SESSION['setup']['db_converted'] = true;
 		}
